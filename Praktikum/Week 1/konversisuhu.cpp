1904c630a6225928e35c81abffc53bf607ca4158
FakhriNaabilPrasetyo <fahrinabil107@gmail.com>
2025-09-30 15:54:21 +0700

    pertama

---
+++ Praktikum/Week 1/konversisuhu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float celcius,fahrenheit;
+    cout<<"Tulis sedingin apa pacar lu:";
+    cin>>celcius;
+    fahrenheit=(9.0/5.0)*celcius + 32;
+    cout<<"Kalau di fahrenheit itu " << fahrenheit << " derajat" <<endl;
+    cout<<"itu dingin banget sihh, saran guwe deketin";
+    return 0;
+}
